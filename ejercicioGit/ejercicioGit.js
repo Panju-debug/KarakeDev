716ede418cd34aba30ae3c1d684f1c4c28926982
Panju-debug <pablovizuete13@gmail.com>
2024-08-15 09:05:50 -0500

    el boton modifica la imagen

--- ejercicioGit/ejercicioGit.js
+++ ejercicioGit/ejercicioGit.js
@@ -1,3 +1,7 @@
 saludar=function(){
     console.log ("ingresa a saludar")
+    let cmpImagenSaludo;
+    cmpImagenSaludo= document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/be53fc5350400539c44b3bc7c2552fe9.gif"
+
 }
