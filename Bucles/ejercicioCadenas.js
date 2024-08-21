6402daffa8ea89b9edfba4500d4f147839566fe6
Panju-debug <pablovizuete13@gmail.com>
2024-08-21 16:55:54 -0500

    Cadenas

---
+++ Bucles/ejercicioCadenas.js
@@ -0,0 +1,8 @@
+recorrerCadena = function (cadena) {
+    let caracter;
+
+  for (let posicion = 0; posicion < cadena.length; posicion++) {
+    caracter=cadena.charAt(posicion);   
+    console.log("caracter: "+caracter+" posicion "+posicion)
+  }     
+}
