a06a0d9f211e7924968f36a95ed175135ec73443
Panju-debug <pablovizuete13@gmail.com>
2024-08-22 10:43:26 -0500

    PASO 1

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,10 +1,44 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
-esMayuscula=function(caracter){
-    let code;
-   code =caracter.charCodeAt()
-   if(code>=65 && code<=90){
+let palabraSecreta;
+
+esMayuscula = function (palabra) {
+  let code;
+  let mayuscula=0;
+
+  for (i = 0; i < palabra.length; i++) {
+    code = palabra.charCodeAt(i);
+    //p 76
+    if (code >= 65 && code <= 90) {
+      mayuscula++;
+    }
+  }
+  if (mayuscula == 5) {
     return true;
-   }else{
+  } else {
     return false;
-   }
-}
+  }
+};
+
+guardarPalabra = function () {
+  let palabra;
+  let mayuscula;
+    /*
+     * Verificar si es mayuscula - esMayuscula -> true o false para uncarater
+     * Realizar un bucle que recorra toda la cadena
+     * Tener un if para verificar si es mayuscula(palabra.chatCode(i))
+     * SI es mayuscula pasa, caso contrario no paso?
+     * SonMayus -> false -  verificar que toda la cadena es mayuscula
+     * Cuando SonMayus es true deja pasar
+     */
+
+  palabra = recuperarTexto("txtSecreta");
+  if (palabra.length == 5) {
+    mayuscula = esMayuscula(palabra);
+    if (mayuscula == false) {
+      alert(" DEBE INGRESAR UNA PALABRA CON 5 LETRAS MAYUSCULAS");
+    } else {
+      palabraSecreta = palabra;
+      console.log(palabraSecreta);
+    }
+  }
+};
